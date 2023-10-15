fa6b087138e1984bb3aac8b7654e9d3aa32b4ad5
Craig Buchanan <craig.buchanan.is@gmail.com>
2023-10-15 18:01:57 +0200

    Initial commit

---
+++ apps/becoming-german/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'becoming-german-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'becoming-german';
+}
