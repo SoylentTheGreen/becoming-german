c6b159f29a7a5117bf943933672c748440f8bf3b
dschoaen <info@m-o-a-r.de>
2023-11-17 22:40:23 +0100

    wip minor changes

--- libs/becoming-german-ui/src/lib/about/about.component.ts
+++ libs/becoming-german-ui/src/lib/about/about.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bgn-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss'],
+  styleUrls: ['./about.component.scss', '../standard-layout/standard-layout.component.scss'],
 })
 export class AboutComponent {}
--- libs/becoming-german-ui/src/lib/project/wanderschaft/wanderschaft.component.ts
+++ libs/becoming-german-ui/src/lib/project/wanderschaft/wanderschaft.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bgn-wanderschaft',
   templateUrl: './wanderschaft.component.html',
-  styleUrls: ['./wanderschaft.component.scss'],
+  styleUrls: ['./wanderschaft.component.scss', '../../standard-layout/standard-layout.component.scss'],
 })
 export class WanderschaftComponent {}
