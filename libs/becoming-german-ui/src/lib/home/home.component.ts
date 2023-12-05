83e6d6fd2c5e98d0f4a7af34a17056f301ca20ad
dschoaen <info@m-o-a-r.de>
2023-12-05 17:24:21 +0100

    wip under construction!

--- libs/becoming-german-ui/src/lib/home/home.component.ts
+++ libs/becoming-german-ui/src/lib/home/home.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'bgn-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  styleUrls: ['./home.component.scss', '../start/start.component.scss'],
 })
 export class HomeComponent {}
