cf494ae6856dea4c482feab0e7eb893fb589e2b2
Craig Buchanan <craig.buchanan.is@gmail.com>
2023-12-13 17:47:42 +0100

    nav layout and responsive design

--- libs/becoming-german-ui/src/lib/nav/nav.component.ts
+++ libs/becoming-german-ui/src/lib/nav/nav.component.ts
@@ -1,11 +1,28 @@
 import { Component, HostBinding } from '@angular/core';
-
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
+import { pipe } from 'fp-ts/function';
+import * as A from 'fp-ts/Array';
+import * as O from 'fp-ts/Option';
 const linkText: () => [string, string][] = () => [
   ['request', $localize`:@@nav-request:Empfangen`],
   ['spenden', $localize`:@@nav-spenden:Spenden`],
   ['project', $localize`:@@nav-project:Projekt-Infos`],
   ['about', $localize`:@@nav-about:Impressum`],
 ];
+const regex = /\/([^\/]*)\/?/;
+const parseUrl = (e: NavigationEnd): string => {
+  return pipe(
+    e.urlAfterRedirects.match(regex),
+    O.fromNullable,
+    O.map(res => res[1]),
+    O.chain(k =>
+      pipe(linkText(), A.findFirst(l => l[0] === k))
+    ),
+    O.fold(() => '', l => l[1])
+  )
+
+};
 
 @Component({
   selector: 'bgn-nav',
@@ -16,4 +33,13 @@ export class NavComponent {
   links = linkText();
   @HostBinding('class.open')
   open = false;
+  current = '';
+  sub = this.router.events
+    .pipe(
+      filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+      map(parseUrl),
+    )
+    .subscribe((r) => (this.current = r));
+
+  constructor(private router: Router) {}
 }
